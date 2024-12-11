8d1530a3a66229a59bccee087108180274fb2cac
borrison-s <boris.stojkoski@gmail.com>
2024-12-11 15:09:58 +0100

    Create IUserRepository interface for user-specific queries

---
+++ ForumApp/ForumApp.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using ForumApp.Domain.Models;
+
+namespace ForumApp.DataAccess.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        // Get all entities of type T
+        List<T> GetAll();
+
+        // Get an entity by its Id
+        T GetById(int id);
+
+        // Add a new entity of type T
+        void Add(T entity);
+
+        // Update an existing entity of type T
+        void Update(T entity);
+
+        // Delete an entity of type T
+        void Delete(T entity);
+    }
+}
